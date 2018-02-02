1222aad878cb89a574d36670eca84bb41c5cc915
FwoopFwoop <zacharysamiyoussef@gmail.com>
2018-02-02 15:05:58 -0500

    Reintroduce lowercase input support for cipher

--- src/main/scala/Cipher.scala
+++ src/main/scala/Cipher.scala
@@ -3,7 +3,7 @@ package main.scala
 object Cipher extends App{
 
   def linearCipher(m: String, k: Int, b: Int) =
-    m.map(c => ((((c.toInt - 65) * k + b) % 26) + 65).toChar)
+    m.map(c => ((((c.toUpper.toInt - 65) * k + b) % 26) + 65).toChar)
 
   print(linearCipher("HELLO", 3, 16))
 }
